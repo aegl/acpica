32d7a5b08704de69891f0a549f761ec12fe059c9
Tony Luck <tony.luck@intel.com>
2025-02-12 13:26:06 -0800

    iASL: MRRM template
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- source/compiler/dtcompiler.h
+++ source/compiler/dtcompiler.h
@@ -821,6 +821,7 @@ extern const unsigned char  TemplateMchi[];
 extern const unsigned char  TemplateMpam[];
 extern const unsigned char  TemplateMpst[];
 extern const unsigned char  TemplateMsct[];
+extern const unsigned char  TemplateMrrm[];
 extern const unsigned char  TemplateMsdm[];
 extern const unsigned char  TemplateNfit[];
 extern const unsigned char  TemplatePcct[];
